67fed705150d06f52433708b32783b83c0d1438f
Pedro Cunegundes <bangue062@gmail.com>
2023-10-03 17:32:01 -0300

    alteracoes

---
+++ media.java
@@ -0,0 +1,12 @@
+public class media{
+    public static void main(String args[]){
+        int num1 = 6;
+        int num2 = 7;
+        int num3 = 3;
+        int num4 = 8;
+
+        int media = (num1 + num2 + num3 + num4)/4;
+
+        System.out.println(media);
+    }
+}
